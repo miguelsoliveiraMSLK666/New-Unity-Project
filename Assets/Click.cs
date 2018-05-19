089ca98851735bb0e15cf908a7b3a02da7785ca3
miguelsoliveiraMSLK666 <39244610+miguelsoliveiraMSLK666@users.noreply.github.com>
2018-05-19 15:57:29 -0300

    Initial commit

---
+++ Assets/Click.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class Click : MonoBehaviour{
+
+    public UnityEngine.UI.Text BitCoin;
+
+    public UnityEngine.UI.Text cpc;
+
+    public float coin = 0.00f;
+
+    public int coinperclick = 1;
+
+    private void Update()
+    {
+
+        BitCoin.text = "Coin: " + coin;
+
+        cpc.text = "CPC: " + coinperclick;
+
+
+    }
+
+    public void Clicked()
+    {
+
+        coin += coinperclick;
+
+    }
+
+}
---
+++ Assets/UpgradeManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+
+    public Click click;
+
+    public UnityEngine.UI.Text itemInfo;
+
+    public float cost;
+
+    public int count = 0;
+
+    public int clickPower;
+
+    public string itemName;
+
+    private float basecost;
+
+    private void Start(){
+
+        basecost = cost;
+
+
+    }
+
+    void Update(){
+
+        itemInfo.text = itemName + "\nCusto: " + cost + "\nPoder: " +"+"+ clickPower;
+
+
+    }
+
+    public void PurchaseUpgrade(){
+
+        if (click.coin >= cost){
+
+
+            click.coin = click.coin - cost;
+
+            count += 1;
+
+            click.coinperclick += clickPower;
+
+            cost =Mathf.Round(basecost * Mathf.Pow(1.15f, count));
+
+        }
+
+    }
+
+
+}
